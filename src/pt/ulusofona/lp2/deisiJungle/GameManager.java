6a35aef65fabe5b3828ad7ee8baf8b901ae3516f
TunaPT <78658365+TunaPT@users.noreply.github.com>
2022-10-30 12:01:46 +0000

    update getSpecies

--- src/pt/ulusofona/lp2/deisiJungle/GameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameManager.java
@@ -7,7 +7,33 @@ public class GameManager {
     public GameManager() {}
 
     public String[][] getSpecies() {
-        return null;
+        //Creates the default species data
+        String[][] species = new String[5][3];
+
+        //SpecieId
+        //SpecieName
+        //SpecieImageFileName
+        species[0][0] = "E";
+        species[0][1] = "Elefante";
+        species[0][2] = "elephant.png";
+
+        species[1][0] = "L";
+        species[1][1] = "Leão";
+        species[1][2] = "lion.png";
+
+        species[2][0] = "T";
+        species[2][1] = "Tartaruga";
+        species[2][2] = "turtle.png";
+
+        species[3][0] = "P";
+        species[3][1] = "Pássaro";
+        species[3][2] = "bird.png";
+
+        species[4][0] = "Z";
+        species[4][1] = "Tarzan";
+        species[4][2] = "tarzan.png";
+
+        return species;
     }
 
     public boolean createInitialJungle(int jungleSize, int initialEnergy, String[][] playersInfo) {
