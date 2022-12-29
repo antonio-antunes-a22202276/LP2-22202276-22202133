be82c824da1e3a8300ee267176e62afbd055ec06
Shadow <jfserralha@gmail.com>
2022-12-29 18:11:16 +0000

    Added Unit Tests

--- src/pt/ulusofona/lp2/deisiJungle/TestGameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/TestGameManager.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import javax.swing.*;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -45,6 +47,483 @@ public class TestGameManager {
         foodsInfo[0][1] = "4";
         InitializationError createJungle = gameManager.createInitialJungle(10, playersInfo);
         assertEquals(null, createJungle);
+    }
+
+    @Test
+    public void test01_getPlayerIds() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[1][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "a";
+        foodsInfo[0][1] = "4";
+        gameManager.createInitialJungle(31, playersInfo, foodsInfo);
+        gameManager.moveCurrentPlayer(4, false);
+
+        int[] playersIds = gameManager.getPlayerIds(4);
+
+        assertEquals(new int[0].length, playersIds.length);
+    }
+
+    @Test
+    public void test02_getPlayerIds() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[1][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "a";
+        foodsInfo[0][1] = "4";
+        gameManager.createInitialJungle(31, playersInfo, foodsInfo);
+        gameManager.moveCurrentPlayer(4, false);
+
+        int[] playersIds = gameManager.getPlayerIds(5);
+
+        assertEquals(1, playersIds[0]);
+    }
+
+    @Test
+    public void test01_getPlayerInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[1][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "a";
+        foodsInfo[0][1] = "4";
+        gameManager.createInitialJungle(31, playersInfo, foodsInfo);
+
+        String[] playerInfo = gameManager.getPlayerInfo(1);
+
+        assertEquals("[1, JogadorTeste1, L, 80, 4..6]", Arrays.toString(playerInfo));
+    }
+
+    @Test
+    public void test02_getPlayerInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[1][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "a";
+        foodsInfo[0][1] = "4";
+        gameManager.createInitialJungle(31, playersInfo, foodsInfo);
+
+        String[] playerInfo = gameManager.getPlayerInfo(3);
+
+        assertEquals(null, playerInfo);
+    }
+
+    @Test
+    public void test01_getSquareInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[4][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "e";
+        foodsInfo[0][1] = "4";
+
+        foodsInfo[1][0] = "a";
+        foodsInfo[1][1] = "5";
+
+        foodsInfo[2][0] = "a";
+        foodsInfo[2][1] = "6";
+
+        foodsInfo[3][0] = "a";
+        foodsInfo[3][1] = "7";
+        gameManager.createInitialJungle(30, playersInfo, foodsInfo);
+
+        String[] squareInfo = gameManager.getSquareInfo(0);
+
+        assertEquals(null, squareInfo);
+    }
+
+    @Test
+    public void test02_getSquareInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[4][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "e";
+        foodsInfo[0][1] = "4";
+
+        foodsInfo[1][0] = "a";
+        foodsInfo[1][1] = "5";
+
+        foodsInfo[2][0] = "a";
+        foodsInfo[2][1] = "6";
+
+        foodsInfo[3][0] = "a";
+        foodsInfo[3][1] = "7";
+        gameManager.createInitialJungle(30, playersInfo, foodsInfo);
+
+        String[] squareInfo = gameManager.getSquareInfo(30);
+
+        assertEquals("[finish.png, Meta, ]", Arrays.toString(squareInfo));
+    }
+
+    @Test
+    public void test03_getSquareInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[4][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "e";
+        foodsInfo[0][1] = "4";
+
+        foodsInfo[1][0] = "a";
+        foodsInfo[1][1] = "5";
+
+        foodsInfo[2][0] = "a";
+        foodsInfo[2][1] = "6";
+
+        foodsInfo[3][0] = "a";
+        foodsInfo[3][1] = "7";
+        gameManager.createInitialJungle(30, playersInfo, foodsInfo);
+
+        String[] squareInfo = gameManager.getSquareInfo(3);
+
+        assertEquals("[image3.png, Vazio, ]", Arrays.toString(squareInfo));
+    }
+
+    @Test
+    public void test04_getSquareInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[4][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "e";
+        foodsInfo[0][1] = "4";
+
+        foodsInfo[1][0] = "a";
+        foodsInfo[1][1] = "5";
+
+        foodsInfo[2][0] = "a";
+        foodsInfo[2][1] = "6";
+
+        foodsInfo[3][0] = "a";
+        foodsInfo[3][1] = "7";
+        gameManager.createInitialJungle(30, playersInfo, foodsInfo);
+
+        String[] squareInfo = gameManager.getSquareInfo(2);
+
+        assertEquals("[image2.png, Vazio, ]", Arrays.toString(squareInfo));
+    }
+
+    @Test
+    public void test05_getSquareInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[4][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "e";
+        foodsInfo[0][1] = "4";
+
+        foodsInfo[1][0] = "a";
+        foodsInfo[1][1] = "5";
+
+        foodsInfo[2][0] = "a";
+        foodsInfo[2][1] = "6";
+
+        foodsInfo[3][0] = "a";
+        foodsInfo[3][1] = "7";
+        gameManager.createInitialJungle(30, playersInfo, foodsInfo);
+
+        String[] squareInfo = gameManager.getSquareInfo(11);
+
+        assertEquals("[image1.png, Vazio, ]", Arrays.toString(squareInfo));
+    }
+
+    @Test
+    public void test06_getSquareInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[4][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "e";
+        foodsInfo[0][1] = "4";
+
+        foodsInfo[1][0] = "a";
+        foodsInfo[1][1] = "6";
+
+        foodsInfo[2][0] = "a";
+        foodsInfo[2][1] = "7";
+
+        foodsInfo[3][0] = "a";
+        foodsInfo[3][1] = "9";
+        gameManager.createInitialJungle(30, playersInfo, foodsInfo);
+
+        gameManager.moveCurrentPlayer(4, false);
+
+        String[] squareInfo = gameManager.getSquareInfo(5);
+
+        assertEquals("[image1.png, Vazio, 1]", Arrays.toString(squareInfo));
+    }
+
+    @Test
+    public void test07_getSquareInfo() { //Line 238
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[4][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "e";
+        foodsInfo[0][1] = "4";
+
+        foodsInfo[1][0] = "a";
+        foodsInfo[1][1] = "6";
+
+        foodsInfo[2][0] = "a";
+        foodsInfo[2][1] = "7";
+
+        foodsInfo[3][0] = "a";
+        foodsInfo[3][1] = "9";
+        gameManager.createInitialJungle(30, playersInfo, foodsInfo);
+
+        gameManager.moveCurrentPlayer(4, false);
+        gameManager.moveCurrentPlayer(7, true);
+
+        String[] squareInfo1 = gameManager.getSquareInfo(5);
+        String[] squareInfo2 = gameManager.getSquareInfo(8);
+        String[] finalSquareInfo = new String[squareInfo1.length + squareInfo2.length];
+        System.arraycopy(squareInfo1, 0, finalSquareInfo, 0, squareInfo1.length);
+        System.arraycopy(squareInfo2, 0, finalSquareInfo, squareInfo1.length, squareInfo2.length);
+
+        assertEquals("[image1.png, Vazio, 1, image2.png, Vazio, 2]", Arrays.toString(finalSquareInfo));
+    }
+
+    @Test
+    public void test08_getSquareInfo() { //Line 238
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[4][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "e";
+        foodsInfo[0][1] = "4";
+
+        foodsInfo[1][0] = "a";
+        foodsInfo[1][1] = "6";
+
+        foodsInfo[2][0] = "a";
+        foodsInfo[2][1] = "7";
+
+        foodsInfo[3][0] = "a";
+        foodsInfo[3][1] = "9";
+        gameManager.createInitialJungle(30, playersInfo, foodsInfo);
+
+        String[] squareInfo = gameManager.getSquareInfo(4);
+
+        assertEquals("[grass.png, Erva : +- 20 energia, ]", Arrays.toString(squareInfo));
+    }
+
+    @Test
+    public void test_getCurrentPlayerInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[1][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "a";
+        foodsInfo[0][1] = "4";
+        gameManager.createInitialJungle(31, playersInfo, foodsInfo);
+
+        String[] playerInfo = gameManager.getCurrentPlayerInfo();
+
+        assertEquals("[1, JogadorTeste1, L, 80, 4..6]", Arrays.toString(playerInfo));
+    }
+
+    @Test
+    public void test_getCurrentPlayerEnergyInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][7];
+        String[][] foodsInfo = new String[1][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "2";
+        playersInfo[0][5] = "10";
+        playersInfo[0][6] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "4";
+        playersInfo[1][5] = "10";
+        playersInfo[1][6] = "1..6";
+
+        foodsInfo[0][0] = "a";
+        foodsInfo[0][1] = "4";
+        gameManager.createInitialJungle(31, playersInfo, foodsInfo);
+
+        String[] playerInfo = gameManager.getCurrentPlayerEnergyInfo(2);
+
+        assertEquals("[4, 10]", Arrays.toString(playerInfo));
+    }
+
+    @Test
+    public void test_getPlayersInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][5];
+        String[][] foodsInfo = new String[1][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+        playersInfo[0][3] = "80";
+        playersInfo[0][4] = "4..6";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+        playersInfo[1][3] = "180";
+        playersInfo[1][4] = "1..6";
+
+        foodsInfo[0][0] = "a";
+        foodsInfo[0][1] = "4";
+        gameManager.createInitialJungle(31, playersInfo, foodsInfo);
+
+        String[][] playerInfo = gameManager.getPlayersInfo();
+
+        assertEquals("[[1, JogadorTeste1, L, 80, 4..6], [2, JogadorTeste2, E, 180, 1..6]]", Arrays.deepToString(playerInfo));
     }
 
     @Test
@@ -166,6 +645,64 @@ public class TestGameManager {
         gameManager.moveCurrentPlayer(5, false); //Turtle
         MovementResult outputMovement = gameManager.moveCurrentPlayer(4, false); //Lion
         assertEquals(new MovementResult(MovementResultCode.CAUGHT_FOOD, "Apanhou Carne"), outputMovement);
+    }
+
+    @Test
+    public void test01_getWinnerInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][3];
+        String[][] foodsInfo = new String[1][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+
+        foodsInfo[0][0] = "a";
+        foodsInfo[0][1] = "27";
+        gameManager.createInitialJungle(30, playersInfo, foodsInfo);
+        gameManager.moveCurrentPlayer(0, false); //Lion
+        gameManager.moveCurrentPlayer(4, false); //Elefante
+        gameManager.moveCurrentPlayer(0, false); //Lion
+        gameManager.moveCurrentPlayer(4, false); //Elefante
+        gameManager.moveCurrentPlayer(0, false); //Lion
+        gameManager.moveCurrentPlayer(4, false); //Elefante
+        gameManager.moveCurrentPlayer(0, false); //Lion
+        gameManager.moveCurrentPlayer(4, true); //Elefante
+
+        String[] gameResults = gameManager.getWinnerInfo();
+        String finalGameResults = "";
+        finalGameResults += gameResults[0] + ", " + gameResults[1] + ", " + gameResults[2] + ", " + gameResults[3] + ", " + gameResults[4];
+
+        assertEquals("1, JogadorTeste1, L, 120, 4..6", finalGameResults);
+    }
+
+    @Test
+    public void test02_getWinnerInfo() {
+        GameManager gameManager = new GameManager();
+        String[][] playersInfo = new String[2][3];
+        String[][] foodsInfo = new String[1][2];
+        playersInfo[0][0] = "1";
+        playersInfo[0][1] = "JogadorTeste1";
+        playersInfo[0][2] = "L";
+
+        playersInfo[1][0] = "2";
+        playersInfo[1][1] = "JogadorTeste2";
+        playersInfo[1][2] = "E";
+
+        foodsInfo[0][0] = "a";
+        foodsInfo[0][1] = "27";
+        gameManager.createInitialJungle(30, playersInfo, foodsInfo);
+        gameManager.moveCurrentPlayer(0, false); //Lion
+        gameManager.moveCurrentPlayer(4, false); //Elefante
+        gameManager.moveCurrentPlayer(0, false); //Lion
+        gameManager.moveCurrentPlayer(4, false); //Elefante
+
+        String[] gameResults = gameManager.getWinnerInfo();
+
+        assertEquals(null, gameResults);
     }
 
     @Test
